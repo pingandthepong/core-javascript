f0993ab478a48905fe53742171ecfa40be09cc3e
pingandthepong <pingandthepong@gmail.com>
2022-12-29 13:15:25 +0900

    if문과 삼항연산자 예제

--- client/chapter/core/08-1.condition.js
+++ client/chapter/core/08-1.condition.js
@@ -4,7 +4,7 @@
 
 // if..else와 prompt를 사용해 0보다 크면 1, 0보다 작으면 -1, 0을 받으면 0 출력
 
-let number = prompt("숫자를 입력해주세요", 0);
+// let number = prompt("숫자를 입력해주세요", 0);
 
 // if..else 사용
 /* if (number > 0) {
@@ -16,8 +16,8 @@ let number = prompt("숫자를 입력해주세요", 0);
 } */
 
 // 삼항연산자 사용
-let message = number > 0 ? "1" : number < 0 ? "-1" : "아무것도 아닙니다!";
-console.log(message);
+/* let message = number > 0 ? "1" : number < 0 ? "-1" : "아무것도 아닙니다!";
+console.log(message); */
 
 // alert(message);
 
@@ -29,12 +29,37 @@ console.log(message);
 //       Yes | No
 
 // 영화 봤니?
-let didWatchMovie;
+let didWatchMovie = "no";
 
 // 영화 볼거니?
-let goingToWatchMovie;
+let goingToWatchMovie = "yes";
 
 // if 문(statement)
+
+/* let movie = prompt("영화를 보았습니까?", "yes or no?");
+
+if (movie == "yes") {
+  console.log(didWatchMovie);
+} else if (movie == "no") {
+  console.log(goingToWatchMovie);
+} else {
+  console.log("잘못된 값을 입력했습니다.");
+}
+ */
+
+if (didWatchMovie === "yes") {
+  console.log("그거 재밌더라");
+} else if (goingToWatchMovie === "yes") {
+  console.log("너무 설렌다~");
+} else {
+  console.log("음.. 난 별로..");
+}
+
+let movieMessage = didWatchMovie.includes("yes")
+  ? "그거 재밌더라"
+  : goingToWatchMovie === "yes"
+  ? "너무 설렌다~"
+  : "난 별로..";
 
 // else 절(caluse)
 
