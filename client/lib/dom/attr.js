706cd550c98a3fc6535c5c732fc11e5e73e6ce3e
pingandthepong <pingandthepong@gmail.com>
2023-01-13 13:39:09 +0900

    entries 파일 추가

--- client/lib/dom/attr.js
+++ client/lib/dom/attr.js
@@ -1,3 +1,5 @@
+import { getNode } from "./getNode.js";
+
 /* 
 
 // IIFE 패턴 
@@ -73,7 +75,7 @@ function getAttr(node, prop) {
   // node = '.first'
   // prop = 'class'
 
-  if (typeof node === 'string') {
+  if (typeof node === "string") {
     node = getNode(node);
   }
 
@@ -83,19 +85,19 @@ function getAttr(node, prop) {
 // computed property
 function setAttr(node, prop, value) {
   // validation : 확인
-  if (typeof node === 'string') node = getNode(node);
-  if (typeof prop !== 'string')
+  if (typeof node === "string") node = getNode(node);
+  if (typeof prop !== "string")
     throw new TypeError(
-      'setAttr 함수의 두 번째 인자는 문자 타입 이어야 합니다.'
+      "setAttr 함수의 두 번째 인자는 문자 타입 이어야 합니다."
     );
 
-  if (prop.includes('data')) {
+  if (prop.includes("data")) {
     let rest = prop.slice(5);
     node.dataset[rest] = value;
   }
 
   if (!value)
-    throw new SyntaxError('setAttr 함수의 세 번째 인자는 필수값입니다.');
+    throw new SyntaxError("setAttr 함수의 세 번째 인자는 필수값입니다.");
 
   node.setAttribute(prop, value);
 }
