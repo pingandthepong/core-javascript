0f708ec0bffd118bbf07b68e15144e8b8017bc7e
pingandthepong <pingandthepong@gmail.com>
2023-01-17 15:38:50 +0900

    콜백 함수 생성 및 실행

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -2,7 +2,18 @@ import { getNode } from "../dom/getNode.js";
 
 const first = getNode(".first");
 
-first.style.top = "-100px";
-first.style.transform = "rotate(360deg)";
-// 콜백이 필요해진 시점
-first.style.top = "0px";
+// 콜백 함수 생성
+function delay(callback, timeout = 1000) {
+  setTimeout(callback, timeout);
+}
+
+// 콜백 함수 사용 (콜백 지옥)
+delay(() => {
+  first.style.top = "-100px";
+  delay(() => {
+    first.style.transform = "rotate(360deg)";
+    delay(() => {
+      first.style.top = "0px";
+    });
+  });
+});
