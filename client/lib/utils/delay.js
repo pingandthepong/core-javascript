cbe35f35f6b853c0c4b14d689d9d222e6e61197a
pingandthepong <pingandthepong@gmail.com>
2023-01-18 10:14:59 +0900

    catch로 에러 잡기 (메시지 띄우기)

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -21,51 +21,39 @@ const second = getNode(".second");
 // 많아지면 보기 힘듦. 그래서 프라미스가 필요. 가독성있게 만들 수 있음!
 // 프라미스 사용 이용 = 콜백 가독성을 위해
 
-delayP()
-  .then(() => {
-    first.style.top = "-100px";
-    return delayP();
-  })
-  .then(() => {
-    first.style.transform = "rotate(360deg)";
-    second.style.left = "100px";
-    return delayP();
-  })
-  .then(() => {
-    first.style.top = "0px";
-    second.style.left = "0px";
-  });
+// delayP()
+//   .then(() => {
+//     first.style.top = "-100px";
+//     return delayP();
+//   })
+//   .then(() => {
+//     first.style.transform = "rotate(360deg)";
+//     second.style.left = "100px";
+//     return delayP();
+//   })
+//   .then(() => {
+//     first.style.top = "0px";
+//     second.style.left = "0px";
+//   });
 
 // Promise 로 약속 -> Then 으로 받아서 사용
 function delayP(timeout = 1000) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("성공!");
-      // reject("실패!");
+      // resolve("성공!");
+      reject("실패!");
     }, timeout);
   });
 }
 // console.log(delayP());
 
-
 // 프라미스 체이닝 설멍 (지금은 던져주는 게 없어서 undefined 나올거임)
 // then에서 return을 던져줘야 함!
 // 귀찮나? 그래서 나중에 async await이 나옴..
-delayP().then((res) => {
-  console.log(res);
-});
-.then((res) => {
-  console.log(res);
-});
-.then((res) => {
-  console.log(res);
-});
-.then((res) => {
-  console.log(res);
-});
-.then((res) => {
-  console.log(res);
-});
-.then((res) => {
-  console.log(res);
-});
+delayP()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
