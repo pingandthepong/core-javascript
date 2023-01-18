a7fd210e751c8916841acf8b8aacf91b42264b06
pingandthepong <pingandthepong@gmail.com>
2023-01-18 10:09:15 +0900

    콜백지옥을 Promise로

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -1,24 +1,40 @@
 import { getNode } from "../dom/getNode.js";
 
 const first = getNode(".first");
+const second = getNode(".second");
 
 // 콜백 함수 생성
-function delay(callback, timeout = 1000) {
-  setTimeout(callback, timeout);
-}
+// function delay(callback, timeout = 1000) {
+//   setTimeout(callback, timeout);
+// }
 
 // 콜백 함수 사용 (콜백 지옥)
-delay(() => {
-  first.style.top = "-100px";
-  delay(() => {
-    first.style.transform = "rotate(360deg)";
-    delay(() => {
-      first.style.top = "0px";
-    });
-  });
-});
+// delay(() => {
+//   first.style.top = "-100px";
+//   delay(() => {
+//     first.style.transform = "rotate(360deg)";
+//     delay(() => {
+//       first.style.top = "0px";
+//     });
+//   });
+// });
 // 많아지면 보기 힘듦. 그래서 프라미스가 필요. 가독성있게 만들 수 있음!
 // 프라미스 사용 이용 = 콜백 가독성을 위해
+
+delayP()
+  .then(() => {
+    first.style.top = "-100px";
+    return delayP();
+  })
+  .then(() => {
+    first.style.transform = "rotate(360deg)";
+    second.style.left = "100px";
+    return delayP();
+  })
+  .then(() => {
+    first.style.top = "0px";
+    second.style.left = "0px";
+  });
 
 // Promise 로 약속 -> Then 으로 받아서 사용
 function delayP(timeout = 1000) {
@@ -29,4 +45,8 @@ function delayP(timeout = 1000) {
     }, timeout);
   });
 }
-console.log(delayP());
+// console.log(delayP());
+
+delayP().then((res) => {
+  console.log(res);
+});
