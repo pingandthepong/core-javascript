666755906210e948395d98c85f4269190b8ee91e
pingandthepong <pingandthepong@gmail.com>
2023-01-19 09:17:11 +0900

    CRUD

--- client/lib/utils/tiger.js
+++ client/lib/utils/tiger.js
@@ -5,26 +5,21 @@ const defaultOptions = {
   cache: "no-cache",
   credential: "same-origin",
   redirect: "follow",
-  referrerPolicy: "no-reffere",
+  referrerPolicy: "no-referrer",
   headers: {
     "Content-Type": "application/json; charset=UTF-8",
   },
 };
 
 // fetch 쓸거임
-const tiger = async (options = {}) => {
+export const tiger = async (options = {}) => {
   const { url, ...restOptions } = {
     ...defaultOptions,
     ...options,
     headers: { ...defaultOptions.headers, ...options.headers },
   };
 
-  let response = await fetch("https://jsonplaceholder.typicode.com/users/1", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  let response = await fetch(url, restOptions);
   if (response.ok) {
     response.data = await response.json();
   }
@@ -33,4 +28,58 @@ const tiger = async (options = {}) => {
 };
 // console.log(await tiger());
 
-tiger({});
+// tiger({});
+
+/**
+ * @function tiger
+ * @param {string} url
+ * @param {object} body
+ * @param {{a:number, b:string, c:string}} options
+ * @returns {Promise} promise
+ */
+
+tiger.get = async (url, options) => {
+  return tiger({
+    url,
+    ...options,
+  });
+};
+
+tiger.post = (url, body, options) => {
+  return tiger({
+    method: "POST",
+    url,
+    body: JSON.stringify(body),
+    ...options,
+  });
+};
+
+tiger.put = (url, body, options) => {
+  return tiger({
+    method: "PUT",
+    url,
+    body: JSON.stringify(body),
+    ...options,
+  });
+};
+
+tiger.delete = (url, options) => {
+  return tiger({
+    method: "DELETE",
+    url,
+    body: JSON.stringify(body),
+    ...options,
+  });
+};
+
+// 이렇게 사용함
+// tiger.post(
+//   "www.naver.com",
+//   { name: "tiger" },
+//   {
+//     mode: "cors",
+//     headers: {},
+//     cache: "no-cache",
+//     credential: "same-origin",
+//   }
+// );
